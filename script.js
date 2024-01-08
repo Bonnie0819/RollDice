66c7d9dac1c3452d4fdd38e8f333b5fca9f39429
Bonnie Chen <114087522+Bonnie0819@users.noreply.github.com>
2024-01-08 15:29:46 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ function initialize() {
     inputContainer = document.getElementById("inputform");
     dice = "";
     rolls = "";
+    die1 = 0;
+    die2 = 0;
+    die3 = 0;
+    total = 0;
+    diceVal = [];
 
 }
 //records the data from the form
@@ -16,8 +21,29 @@ function recordData() {
 }
 //displays data on the webpage
 function display() {
-    console.log(dice);
-    console.log(rolls);
     document.getElementById("dice").innerHTML = "Number of Dice: " + dice;
     document.getElementById("rolls").innerHTML = "Number of Rolls:" + rolls;
+}
+
+function roll() {
+    for(let i = 0; i < rolls; i++) {
+        if(dice === 1) {
+            die1= Math.random() *6 + 1;
+        }
+        if(dice === 2) {
+            die1= Math.random() *6 + 1;
+            die2= Math.random() *6 + 1;
+        }
+        if(dice === 3) {
+            die1= Math.random() *6 + 1;
+            die2= Math.random() *6 + 1;
+            die3= Math.random() *6 + 1;
+        }
+        total = die1 + die2 + die3;
+        console.log(die1);
+        console.log(die2);
+        console.log(die3);
+        console.log(total);
+    }
+
 }
