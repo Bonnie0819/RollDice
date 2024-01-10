4c8b2b9d0ddc3e9bdf50336910f0df89fc15fc17
Bonnie Chen <114087522+Bonnie0819@users.noreply.github.com>
2024-01-10 15:04:06 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -6,9 +6,18 @@ function initialize() {
     die2 = 0;
     die3 = 0;
     total = 0;
-    diceVal = [];
-
+    die1Val =[];
+    die2Val =[];
+    die3Val =[];
+    totalVal =[];
+    t = document.getElementById("frequency");
 }
+//displays data on the webpage
+function display() {
+    document.getElementById("dice").innerHTML = "Number of Dice: " + dice;
+    document.getElementById("rolls").innerHTML = "Number of Rolls: " + rolls;
+}
+
 //records the data from the form
 function recordData() {
     dice = inputContainer.numDice.value;
@@ -17,33 +26,44 @@ function recordData() {
     inputContainer.numDice.value = "";
     inputContainer.rolls.value = "";
 
-    display();
+    roll(rolls);
 }
-//displays data on the webpage
-function display() {
+
+//simulates dice rolling
+function roll(num) {
     document.getElementById("dice").innerHTML = "Number of Dice: " + dice;
-    document.getElementById("rolls").innerHTML = "Number of Rolls:" + rolls;
+    document.getElementById("rolls").innerHTML = "Number of Rolls: " + rolls;
+    for(let i = 0; i < num; i++) {
+        die1= parseInt(Math.random() *6 + 1);
+        die2= parseInt(Math.random() *6 + 1);
+        die3= parseInt(Math.random() *6 + 1);
+        total = die1 + die2 + die3;
+
+        //pushes the data to an array
+        die1Val.push(die1);
+        die2Val.push(die2);
+        die3Val.push(die3);
+        totalVal.push(total);
+    }
+    table(dice);
 }
 
-function roll() {
-    for(let i = 0; i < rolls; i++) {
-        if(dice === 1) {
-            die1= Math.random() *6 + 1;
-        }
-        if(dice === 2) {
-            die1= Math.random() *6 + 1;
-            die2= Math.random() *6 + 1;
-        }
-        if(dice === 3) {
-            die1= Math.random() *6 + 1;
-            die2= Math.random() *6 + 1;
-            die3= Math.random() *6 + 1;
-        }
-        total = die1 + die2 + die3;
-        console.log(die1);
-        console.log(die2);
-        console.log(die3);
-        console.log(total);
-    }
+//generates the table of frequencies
+//Used https://www.tutorialspoint.com/How-to-add-rows-to-a-table-using-JavaScript-DOM for help
+function table(num) {
+    for(let i = num; i <= num*6; i++) {
+        let row= t.insertRow(t.rows.length);
+        let c1 = row.insertCell(0);
+        let c2 = row.insertCell(1);
 
+        c1.innerText = i;
+
+        let f = 0;
+        for(let j = 0; j < totalVal.length; j++) {
+            if(totalVal[j] === i) {
+                f++;
+            }
+        }
+        c2.innerText = f;
+    }
 }
